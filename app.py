4bd8945297da8745642f22603c8c8517792aff1d
flyingculprit <suryan2021ai@gmail.com>
2025-05-11 14:51:48 +0530

    Commander finish off on his style

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ CLIENT = InferenceHTTPClient(
 )
 
 # Gemini setup
-genai.configure(api_key="AIzaSyD5EVduUfkCnbj_fWs2-Lci18Eq7necSYs")
+genai.configure(api_key="s")
 model = genai.GenerativeModel("gemini-1.5-flash")
 
 def get_grocery_info(grocery_name):
